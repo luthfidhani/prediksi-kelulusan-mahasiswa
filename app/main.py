cb316ba5d689f8cf4f988a9c64fd7a18a88c34a6
Luthfi A. Ardhani <luthfidhani17@gmail.com>
2024-05-02 21:02:55 +0700

    💡 feat: add some params

--- app/main.py
+++ app/main.py
@@ -1,34 +1,77 @@
 import os
 import joblib
 import pandas as pd
+import numpy as np
 from flask import Flask, render_template, request
-
+from vars import jalur, prodi, jenis_kelamin
 
 app = Flask(__name__)
 
 model = joblib.load(os.path.join(os.path.dirname(__file__), "model_logistik.pkl"))
 
 
-def predict(jumlah_semester, jenjang):
-    df = pd.DataFrame({"Jumlah_Semester": [jumlah_semester], "jenjang": [jenjang]})
+def predict(n_toefl, ipk, jumlah_semester, prodi_code, jenis_kelamin_code, jalur_code):
+    df = pd.DataFrame(
+        {
+            "n_toefl": [n_toefl],
+            "ipk": [ipk],
+            "jumlah_semester": [jumlah_semester],
+            "prodi_code": [prodi_code],
+            "jenis_kelamin_code": [jenis_kelamin_code],
+            "jalur_code": [jalur_code],
+        },
+    )
     return model.predict(df)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    data = {
+        "vars": {
+            "jalur": jalur,
+            "prodi": prodi,
+            "jenis_kelamin": jenis_kelamin,
+        },
+        "request": [None],
+        "prediction": [None],
+    }
     if request.method == "POST":
-        jumlah_semester = int(request.form["jumlah_semester"])
-        jenjang = request.form["jenjang"]
+        asal_sma = request.form["asal_sma"]
+        n_toefl = int(request.form["n_toefl"])
+        prodi_code = int(request.form["prodi_code"])
+        jenis_kelamin_code = int(request.form["jenis_kelamin_code"])
+        jalur_code = int(request.form["jalur_code"])
 
-        data = {
-            "jumlah_semester": jumlah_semester,
-            "jenjang": jenjang,
-            "prediction": predict(jumlah_semester, jenjang)[0],
-        }
+        ips = []
+        for i in range(1, 15):
+            ip = request.form[f"ip_semester_{i}"]
+            if ip:
+                ips.append(float(request.form[f"ip_semester_{i}"]))
+        ips = np.array(ips, dtype=float)
+   
+        data["request"] = {
+            "asal_sma": asal_sma,
+            "n_toefl": n_toefl,
+            "ipk": np.mean(ips),
+            "jumlah_semester": len(ips),
+            "prodi_code": prodi_code,
+            "jenis_kelamin_code": jenis_kelamin_code,
+            "jalur_code": jalur_code,
+            "ips": ips
+        },
+
+        data["prediction"] = predict(
+            n_toefl=n_toefl,
+            ipk=np.mean(ips),
+            jumlah_semester=len(ips),
+            prodi_code=prodi_code,
+            jenis_kelamin_code=jenis_kelamin_code,
+            jalur_code=jalur_code,
+        )[0]
 
         return render_template("index.html", data=data)
-    return render_template("index.html", data=None)
+    return render_template("index.html", data=data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ app/vars.py
@@ -0,0 +1,31 @@
+jalur = [
+    ('PMDK', 1),
+    ('USMI', 2),
+    ('USM2', 3),
+    ('USM3', 4),
+    ('USM', 5),
+    ('USM 1', 6),
+    ('USM 2', 7),
+    ('USM 3', 8),
+    ('USM 2A', 9),
+    ('USM 2B', 10),
+    ('USM 3A', 11),
+    ('USM 3B', 12),
+    ('UTBK', 13),
+]
+
+prodi = [
+    ("S1 - Informatika", 3),
+    ("S1 - Sistem Informasi", 4),
+    ("S1 - Teknik Elektro", 6),
+    ("S1 - Manajemen Rekayasa", 7),
+    ("S1 - Teknik Bioproses", 8),
+    ("D4 - Sarjana Terapan Teknologi Rekayasa Perangkat Lunak", 2),
+    ("D3 - Teknologi Informasi", 1),
+    ("D3 - Teknologi Komputer", 5),
+]
+
+jenis_kelamin = [
+    ("Laki - Laki", 0),
+    ("Perempuan", 1),
+]
